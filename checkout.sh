7a210daf5f2359c01513b9cb77f037e221878a23
Adam Balsam <balsama@users.noreply.github.com>
2018-01-17 21:23:36 -0500

    Add CI for automatic generation of tags and branches (#1)

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/bash
+set -e
+
+# Decrypt and add private key.
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis-lightning_strict.enc -out travis-lightning_strict -d
+chmod 600 travis-lightning_strict
+eval `ssh-agent -s`
+ssh-add travis-lightning_strict
+
+# Checkout the source (Lightning) and metapackage (this repo)
 mkdir -p tmp
 
 if [ ! -d "tmp/lightning" ]; then
@@ -6,5 +19,5 @@ if [ ! -d "tmp/lightning" ]; then
 fi
 
 if [ ! -d "tmp/metapackage" ]; then
-  git clone --branch master git@github.com:balsama/lightning_strict.git tmp/metapackage
+  git clone --branch ${TRAVIS_BRANCH} git@github.com:balsama/lightning_strict.git tmp/metapackage
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/env sh
+#!/bin/bash
+set -e
+
 cd tmp/metapackage
+
+git checkout ${TRAVIS_BRANCH}
+
+# Decrypt and add private key.
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis-lightning_strict.enc -out travis-lightning_strict -d
+chmod 600 travis-lightning_strict
+eval `ssh-agent -s`
+ssh-add travis-lightning_strict
+
 git push --all origin
 git push --tags origin
